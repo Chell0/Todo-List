7976de6316ca1ab81e9ab0e87c31804518ff10e8
chell0 <machelgabriel@gmail.com>
2019-03-13 10:52:38 +0300

    Delete a Todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").click(function () {
     $(this).toggleClass("completed");
 });
+
+// Click on X  to delete Todo
+$("span").click(function (e) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
