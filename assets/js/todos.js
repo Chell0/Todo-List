ddb31ea24f4255e2a43ed1560b5edd237b1fa710
Chell0 <machelgabriel@gmail.com>
2019-03-14 12:19:49 +0300

    Add a trash icon when creating a new Todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,27 +1,31 @@
 // Check off specific Todos By clicking
-$("ul").on("click", "li", function () {
-    $(this).toggleClass("completed");
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
 });
 
 // Click on X  to delete Todo
-$("ul").on("click", "span", function (e) {
-    $(this).parent().fadeOut(500, function () {
-        $(this).remove();
+$("ul").on("click", "span", function(e) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
     });
-    e.stopPropagation();
+  e.stopPropagation();
 });
 
 // Create a Todo
-$("input[type='text']").keypress(function (e) {
-    if (e.which === 13) {
-        // grab a new todo text from input
-        var todoText = $(this).val();
-        $(this).val("");
-        // create a new li and add to ul
-        $("ul").append("<li><span>X</span> " + todoText + "</li>");
-    }
+$("input[type='text']").keypress(function(e) {
+  if (e.which === 13) {
+    // grab a new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    // create a new li and add to ul
+    $("ul").append(
+      "<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li >"
+    );
+  }
 });
 
-$(".fa-plus").click(function () {
-    $("input[type='text']").fadeToggle();
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle();
 });
